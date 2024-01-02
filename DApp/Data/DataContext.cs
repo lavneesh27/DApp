6d321bf83b1faee468a0a8edd3fe55784b67767d
Bhavesh <Bhavesh@mail.com>
2024-01-02 17:30:35 +0530

    first commit

---
+++ DApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
